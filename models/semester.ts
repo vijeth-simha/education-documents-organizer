4dac6fb8d635e80dfeef2aec28a38f5a8671fdef
vijeth_simha <vijethsimha71@gmail.com>
2023-08-03 00:13:59 +0530

    semester modelling started

---
+++ models/semester.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export default class Semester {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  shortDesction: string;
+
+  @Column()
+  createdAt: Date
+
+  @Column()
+  updatedAt: Date
+  
+}
