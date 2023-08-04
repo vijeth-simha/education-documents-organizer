63a35a937eba8a33a525454ad2b386bd8f6870b5
vijeth_simha <vijethsimha71@gmail.com>
2023-08-05 00:01:34 +0530

    semester crud done

--- controllers/semesterController.ts
+++ controllers/semesterController.ts
@@ -20,31 +20,31 @@ const createSemester = async (req: Request, res: Response) => {
     }
   };
   
-  // const getAllSemesters = async(req:Request,res:Response)=> {
-  //   const semesterRepository = AppDataSource.getRepository(Semester);
-  //   try {
-  //     const semesters:Semester[] = await semesterRepository.find();
-  //     res.status(STATUS_CODES.success).send(semesters);
-  //   } catch (error) {    
-  //     res.status(STATUS_CODES.error).send("Internal Server Error");
-  //   }
-  // }
+  const getAllSemesters = async(req:Request,res:Response)=> {
+    const semesterRepository = AppDataSource.getRepository(Semester);
+    try {
+      const semesters:Semester[] = await semesterRepository.find();
+      res.status(STATUS_CODES.success).send(semesters);
+    } catch (error) {    
+      res.status(STATUS_CODES.error).send("Internal Server Error");
+    }
+  }
   
   
-  // const deleteSemester = async (req: Request, res: Response) => {
-  //   const semesterRepository = AppDataSource.getRepository(Semester);
-  //   const { id } = req.params;
-  //   try {
-  //     await semesterRepository.delete(Number(id));
-  //     res.status(STATUS_CODES.success).send("Semester Deleted Successfully");
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const deleteSemester = async (req: Request, res: Response) => {
+    const semesterRepository = AppDataSource.getRepository(Semester);
+    const { id } = req.params;
+    try {
+      await semesterRepository.delete(Number(id));
+      res.status(STATUS_CODES.success).send("Semester Deleted Successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 
 module.exports={
     createSemester,
-    // getAllSemesters,
-    // deleteSemester
+    getAllSemesters,
+    deleteSemester
 }
