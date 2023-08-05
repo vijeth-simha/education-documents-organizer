79f4e6f77ad05d92da30d098604c299c159ffda8
vijeth_simha <vijethsimha71@gmail.com>
2023-08-05 21:40:22 +0530

    lesson modelling and controller done

--- controllers/lessonController.ts
+++ controllers/lessonController.ts
@@ -2,29 +2,29 @@ import { Request,Response } from "express";
 import { STATUS_CODES } from "../constants/constants";
 import { AppDataSource } from "../db";
 import { MulterRequest } from "../interfaces";
-import { Subject } from "../models";
+import { Lesson } from "../models";
 
-const createSubject = async (req: Request, res: Response) => {
+const createLesson = async (req: Request, res: Response) => {
     // const { filename } = (req as MulterRequest).file;
-    const subjectObject: Subject = req.body;
+    const lessonObject: Lesson = req.body;
   
-    const subjectRepository = AppDataSource.getRepository(Subject);
-    subjectObject.createdAt = new Date();
-    // subjectObject.subjectPic = filename;
+    const lessonRepository = AppDataSource.getRepository(Lesson);
+    lessonObject.createdAt = new Date();
+    // lessonObject.subjectPic = filename;
     try {
-      await subjectRepository.save(subjectObject);
-      res.status(STATUS_CODES.success).send("Subject for the semester created successfully");
+      await lessonRepository.save(lessonObject);
+      res.status(STATUS_CODES.success).send("Lesson for the subject created successfully");
     } catch (error) {
       console.log(error);
       res.status(STATUS_CODES.error).send("Internal Server Error");
     }
   };
   
-//   const getAllSemesters = async(req:Request,res:Response)=> {
-//     const subjectRepository = AppDataSource.getRepository(Semester);
+//   const getAllLesson = async(req:Request,res:Response)=> {
+//     const lessonRepository = AppDataSource.getRepository(Lesson);
 //     try {
-//       const semesters:Semester[] = await subjectRepository.find();
-//       res.status(STATUS_CODES.success).send(semesters);
+//       const lesson:Lesson[] = await lessonRepository.find();
+//       res.status(STATUS_CODES.success).send(Lesson);
 //     } catch (error) {    
 //       res.status(STATUS_CODES.error).send("Internal Server Error");
 //     }
@@ -44,7 +44,7 @@ const createSubject = async (req: Request, res: Response) => {
 
 
 module.exports={
-    createSubject,
-    // getAllSemesters,
+    createLesson,
+    // getAllLesson,
     // deleteSemester
 }
---
+++ routes/api/lesson/lesson.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { FileType } from "../../../interfaces";
+const { verifyAccessToken } = require("../../../helpers/JWTHelperUtils");
+const semesterController = require("../../../controllers/semesterController");
+
+const LessonAPIRoutes = Router();
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req: Request, file: File, cb: any) => {
+    cb(null, "./public/img/lesson-images");
+  },
+  filename: function (req: Request, file: FileType, cb: any) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+LessonAPIRoutes.post(
+  "/create-lesson",
+  verifyAccessToken,
+  upload.single("lessonPic"),
+  semesterController.createLesson
+);
+
+export default LessonAPIRoutes;
