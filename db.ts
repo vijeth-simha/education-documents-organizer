5e2ed98a7ac7d57700ba0c8f65e04759c2e7ef4f
vijeth_simha <vijethsimha71@gmail.com>
2023-08-09 00:26:20 +0530

    document modelling started

--- db.ts
+++ db.ts
@@ -1,15 +1,15 @@
 import { DataSource } from "typeorm";
-import { Category, Lesson, Subject, User } from "./models";
+import { Category, Document, Lesson, Subject, User } from "./models";
 import Semester from "./models/semester";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DATABASE_HOST,
-    port: 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [User,Category,Semester,Subject,Lesson],
-  });
+  type: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Category, Semester, Subject, Lesson, Document],
+});
--- models/index.ts
+++ models/index.ts
@@ -3,3 +3,5 @@ export {default as Category} from './category'
 export {default as Semester} from './semester'
 export {default as Subject} from './subject'
 export {default as Lesson} from './lesson'
+
+export {default as Document} from './document'
