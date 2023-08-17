c538ef6749d1c407cf97c0c43e4984bb8843f201
vijeth_simha <vijethsimha71@gmail.com>
2023-08-18 00:34:40 +0530

    subject view added

--- routes/public/subject/subject.ts
+++ routes/public/subject/subject.ts
@@ -1,25 +1,22 @@
 import { Request, Response, Router } from "express";
 import { AppDataSource } from "../../../db";
-import { Category } from "../../../models";
+import { Subject } from "../../../models";
 
 const SubjectPublicRoutes = Router();
 
-SubjectPublicRoutes.get("/",async (req: Request, res: Response) => {
-  const categoryRepository = AppDataSource.getRepository(Category);
-  const categories:Category[] = await categoryRepository.find();
-  res.render("pages/category/subject-view",{categories});
+SubjectPublicRoutes.get("/", async (req: Request, res: Response) => {
+  const subjectRepository = AppDataSource.getRepository(Subject);
+  const subjectsList: Subject[] = await subjectRepository.find();
+  res.render("pages/subject/subject-view", { subjectsList });
 });
-
 
 SubjectPublicRoutes.get("/create-subject", (req: Request, res: Response) => {
-  res.render("pages/category/create-category");
+  res.render("pages/subject/create-subject");
 });
 
-
-SubjectPublicRoutes.get("/edit-category/:id", (req: Request, res: Response) => {
-  res.render("pages/category/edit-subject");
+SubjectPublicRoutes.get("/edit-subject/:id", (req: Request, res: Response) => {
+  res.render("pages/subject/edit-subject");
 });
-
 
 // module.exports = router;
-export default SubjectPublicRoutes 
+export default SubjectPublicRoutes;
