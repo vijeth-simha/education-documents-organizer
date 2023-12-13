7122f6bb4b1820e1e61e62289529513eb932d6a6
vijeth_simha <vijethsimha71@gmail.com>
2023-12-14 00:01:58 +0530

    starting with lesson

--- routes/public/lesson/lesson.ts
+++ routes/public/lesson/lesson.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express";
 import { AppDataSource } from "../../../db";
-import { Lesson } from "../../../models";
+import { Lesson, Subject } from "../../../models";
 const LessonPublicRoutes = Router();
 
 LessonPublicRoutes.get("/",async (req: Request, res: Response) => {
@@ -10,8 +10,10 @@ LessonPublicRoutes.get("/",async (req: Request, res: Response) => {
 });
 
 
-LessonPublicRoutes.get("/create-lesson", (req: Request, res: Response) => {
-  res.render("pages/category/create-lesson");
+LessonPublicRoutes.get("/create-lesson", async (req: Request, res: Response) => {
+  const subjectRepository = AppDataSource.getRepository(Subject);
+  const subjectsList: Subject[] = await subjectRepository.find();
+  res.render("pages/category/create-lesson",{subjectsList});
 });
 
 
