c3b059c6b05e93d14bb3e0c65901b4a55cc24ebe
vijeth_simha <vijethsimha71@gmail.com>
2023-12-25 23:30:45 +0530

    checking credentials

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,7 +1,7 @@
 import { StatusCode } from "../interfaces";
 
-export const hostname:string = "0.0.0.0";
-export const port:number = 80;
+export const hostname:string = "127.0.0.1";
+export const port:number = 5000;
 export const DUPLICATE_ENTRY_ERRORNO:number = 1062;
 export const JSON_WEB_TOKEN_ERROR:string = "JsonWebTokenError"
 
