39bcff9fda35ec2aa35b708bcd9a1c1f18b3acbe
vijeth_simha <vijethsimha71@gmail.com>
2023-12-28 00:16:45 +0530

    testing ci cd

--- controllers/documentController.ts
+++ controllers/documentController.ts
@@ -60,7 +60,7 @@ const deleteDocument = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
     await documentRepository.delete(Number(id));
-    res.status(STATUS_CODES.success).send("document Deleted Successfully");
+    res.status(STATUS_CODES.success).send("document deleted Successfully");
   } catch (error) {
     console.log(error);
   }
