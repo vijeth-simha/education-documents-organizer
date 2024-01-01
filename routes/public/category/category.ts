3519bd41a78b535cbec4a0bb5fffbf75415cda94
vijeth_simha <vijethsimha71@gmail.com>
2024-01-01 17:21:57 +0530

    added edit image function

--- routes/public/category/category.ts
+++ routes/public/category/category.ts
@@ -4,22 +4,27 @@ import { Category } from "../../../models";
 
 const CategoryPublicRoutes = Router();
 
-CategoryPublicRoutes.get("/",async (req: Request, res: Response) => {
+CategoryPublicRoutes.get("/", async (req: Request, res: Response) => {
   const categoryRepository = AppDataSource.getRepository(Category);
-  const categories:Category[] = await categoryRepository.find();
-  res.render("pages/category/category-view",{categories});
+  const categories: Category[] = await categoryRepository.find();
+  res.render("pages/category/category-view", { categories });
 });
-
 
 CategoryPublicRoutes.get("/create-category", (req: Request, res: Response) => {
   res.render("pages/category/create-category");
 });
 
-
-CategoryPublicRoutes.get("/edit-category/:id", (req: Request, res: Response) => {
-  res.render("pages/category/edit-category");
-});
-
+CategoryPublicRoutes.get(
+  "/edit-category/:id",
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const categoryRepository = await AppDataSource.getRepository(Category);
+    const category: Category | null = await categoryRepository.findOneBy({
+      id: Number(id),
+    });
+    res.render("pages/category/edit-category",{category});
+  }
+);
 
 // module.exports = router;
-export default CategoryPublicRoutes 
+export default CategoryPublicRoutes;
