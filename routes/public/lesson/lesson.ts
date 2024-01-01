95aa6126e66c3df2cf86f605ba5baf421fa6f792
vijeth_simha <vijethsimha71@gmail.com>
2024-01-01 17:56:23 +0530

    edit lesson done

--- routes/public/lesson/lesson.ts
+++ routes/public/lesson/lesson.ts
@@ -3,24 +3,34 @@ import { AppDataSource } from "../../../db";
 import { Lesson, Subject } from "../../../models";
 const LessonPublicRoutes = Router();
 
-LessonPublicRoutes.get("/",async (req: Request, res: Response) => {
+LessonPublicRoutes.get("/", async (req: Request, res: Response) => {
   const lessonRepository = AppDataSource.getRepository(Lesson);
-  const lessonsList:Lesson[] = await lessonRepository.find();
-  res.render("pages/lesson/lesson-view",{lessonsList});
+  const lessonsList: Lesson[] = await lessonRepository.find();
+  res.render("pages/lesson/lesson-view", { lessonsList });
 });
 
+LessonPublicRoutes.get(
+  "/create-lesson",
+  async (req: Request, res: Response) => {
+    const subjectRepository = AppDataSource.getRepository(Subject);
+    const subjectsList: Subject[] = await subjectRepository.find();
+    res.render("pages/lesson/create-lesson", { subjectsList });
+  }
+);
 
-LessonPublicRoutes.get("/create-lesson", async (req: Request, res: Response) => {
-  const subjectRepository = AppDataSource.getRepository(Subject);
-  const subjectsList: Subject[] = await subjectRepository.find();
-  res.render("pages/lesson/create-lesson",{subjectsList});
-});
-
-
-LessonPublicRoutes.get("/edit-semester/:id", (req: Request, res: Response) => {
-  res.render("pages/category/edit-subject");
-});
-
+LessonPublicRoutes.get(
+  "/edit-lesson/:id",
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const lessonRepository = AppDataSource.getRepository(Lesson);
+    const subjectRepository = AppDataSource.getRepository(Subject);
+    const subjectsList: Subject[] = await subjectRepository.find();
+    const lesson: Lesson | null = await lessonRepository.findOneBy({
+      id: Number(id)
+    });
+    res.render("pages/lesson/edit-lesson",{lesson,subjectsList});
+  }
+);
 
 // module.exports = router;
-export default LessonPublicRoutes 
+export default LessonPublicRoutes;
